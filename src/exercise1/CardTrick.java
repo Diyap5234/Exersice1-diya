0ca52b7b0d09db004a10a624a18fbd3fd00c0c9e
Diya Patel <dpatel34922004@gmail.com>
2024-02-06 12:46:14 -0500

    Revert "Revert "changes 1""
    This reverts commit 2f8cf3b047f4ad104929f5942ebbc965acb83afd.

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,4 +1,3 @@
-import java.util.Random;
 import java.util.Scanner;
 
 public class CardTrick {
@@ -6,27 +5,27 @@ public class CardTrick {
     public static void main(String[] args) {
         
         Card[] hand = new Card[7];
-        Random random = new Random();
         Scanner scanner = new Scanner(System.in);
 
         // Fill the hand with random cards
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
-            card.setValue(random.nextInt(13) + 1); // Random value between 1 and 13 (inclusive)
-            card.setSuit(Card.SUITS[random.nextInt(4)]); // Random suit index between 0 and 3
+            card.setValue((int) (Math.random() * 13) + 1); // Random value between 1 and 13 (inclusive)
+            card.setSuit((int) (Math.random() * 4)); // Random suit index between 0 and 3
             hand[i] = card;
         }
 
-        // Ask the user for their guess
-        System.out.println("Enter your guess for a card value (1-13):");
-        int guessValue = scanner.nextInt();
-        System.out.println("Enter your guess for a card suit (1-4):");
-        int guessSuit = scanner.nextInt();
+        // Ask the user to pick a card
+        System.out.println("Pick a card (Enter value and suit):");
+        System.out.print("Enter value (1-13): ");
+        int userValue = scanner.nextInt();
+        System.out.print("Enter suit (1-4): ");
+        int userSuit = scanner.nextInt();
 
-        // Search for a match in the hand
+        // Search for the user's card in the hand
         boolean foundMatch = false;
         for (Card card : hand) {
-            if (card.getValue() == guessValue && card.getSuit() == guessSuit - 1) {
+            if (card.getValue() == userValue && card.getSuit() == userSuit - 1) {
                 foundMatch = true;
                 break;
             }
@@ -36,17 +35,17 @@ public class CardTrick {
         if (foundMatch) {
             printInfo();
         } else {
-            System.out.println("Sorry, no match found.");
+            System.out.println("Sorry, your card is not in the hand.");
         }
     }
 
     /**
      * A simple method to print out personal information.
-     * @author Diya patel
-     * @date 6th feb 2024
+     * @author - diya patel
+     * @date - 6th feb 2024
      */
     private static void printInfo() {
-        System.out.println("Congratulations, you guessed right!");
+        System.out.println("Congratulations, you picked the right card!");
         System.out.println();
         System.out.println("My name is Paul, but you can call me prof, Paul or sir");
         System.out.println();
